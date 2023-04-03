d801d36d403c3cc3825db09714e069c398c60cfb
r-strilets <55804526+r-strilets@users.noreply.github.com>
2023-04-03 23:32:15 +0000

    Deploying to gh-pages from @ r-strilets/Eustoma@d0b914db949d6559fb2805857303023878210455 🚀

--- index.c9987175.js
+++
@@ -1,2 +0,0 @@
-$(document).ready((function(){$(".slider").slick({slidesToScroll:1,autoplay:!0,autoplaySpeed:5e3,slidesToShow:1,infinite:!0,arrows:!1,centerMode:!0,centerPadding:"30px",responsive:[{breakpoint:768,settings:{arrows:!1,centerPadding:"50px",slidesToShow:3}}],mobileFirst:!0})}));
-//# sourceMappingURL=index.c9987175.js.map
